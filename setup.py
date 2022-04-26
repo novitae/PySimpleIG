9ee7fc69b807b0e939e64c9a79efb5ca1dd6d942
аэт <85891169+novitae@users.noreply.github.com>
2022-04-26 18:42:12 +0200

    1.0.5 - Test automated upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pysimpleig',
-    version = '1.0.4',
+    version = '1.0.5',
     description = 'A library to help simplifying the usage instagram',
     author = 'novitae',
     url = 'https://github.com/novitae/PySimpleIG',
